0a86c72bcede4cefc5b356e93e4cb684095f100b
Jordan Harband <ljharb@gmail.com>
2024-03-20 22:15:50 -0700

    [Refactor] use `es-object-atoms` where possible

--- implementation.js
+++ implementation.js
@@ -2,17 +2,17 @@
 
 var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
 var Set = require('es-abstract/2023/Set');
-var ToObject = require('es-abstract/2023/ToObject');
+var ToObject = require('es-object-atoms/ToObject');
 var ToString = require('es-abstract/2023/ToString');
 
 var forEach = require('es-abstract/helpers/forEach');
 
 var callBound = require('call-bind/callBound');
-
 var isString = require('is-string');
+var $Object = require('es-object-atoms');
 
 // Check failure of by-index access of string characters (IE < 9) and failure of `0 in boxedString` (Rhino)
-var boxedString = Object('a');
+var boxedString = $Object('a');
 var splitString = boxedString[0] !== 'a' || !(0 in boxedString);
 
 var strSplit = callBound('String.prototype.split');
